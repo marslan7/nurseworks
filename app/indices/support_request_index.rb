e434222b9a7627fe52dc28612a1c98ca7cf8a1a4
Mohith Thimmaiah <thimmaiah@gmail.com>
2021-05-05 09:35:13 +0530

    Indexes support content

--- app/indices/support_request_index.rb
+++ app/indices/support_request_index.rb
@@ -1,7 +1,8 @@
 ThinkingSphinx::Index.define :support_request, :with => :real_time do
     indexes req_type, :sortable => true
     indexes user.first_name, :as => :first_name, :sortable => true
-  
+    indexes content
+    
     has user_id , :type => :integer 
     has created_at, :type => :timestamp
     has updated_at, :type => :timestamp
