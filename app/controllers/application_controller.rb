0f77981ea4705c980557cd73e105cbc02195d605
Mohith Thimmaiah <thimmaiah@gmail.com>
2021-05-15 10:32:11 +0530

    Added check for authenticity token

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,10 +2,9 @@ class ApplicationController < ActionController::Base
     before_action :authenticate_user!
     before_action :set_search_controller
     before_action :configure_permitted_parameters, if: :devise_controller?
-    skip_before_action :verify_authenticity_token
+    skip_before_action :verify_authenticity_token, if: lambda { ENV["skip_authenticity_token"].present? }
 
     protected
-  
     def configure_permitted_parameters
       devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name, :phone])
     end
