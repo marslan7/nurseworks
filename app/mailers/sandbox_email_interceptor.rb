df817ae0de5eed451896bcb72563e535d5ba743e
Mohith Thimmaiah <thimmaiah@gmail.com>
2021-06-21 21:16:00 +0530

    Removed email from interceptors

--- app/mailers/sandbox_email_interceptor.rb
+++ app/mailers/sandbox_email_interceptor.rb
@@ -1,9 +1,9 @@
 class SandboxEmailInterceptor
     def self.delivering_email(message)
       if ( ['staging'].include?(Rails.env) )
-        message.to = ['support@caremountain.com','thimmaiah@gmail.com', 'gagan.bhalla@gmail.com'] 
+        message.to = ['support@caremountain.com', 'gagan.bhalla@gmail.com'] 
       else
-        message.bcc = ['support@caremountain.com', 'thimmaiah@gmail.com', 'help.caremountain@gmail.com']
+        message.bcc = ['support@caremountain.com', 'help.caremountain@gmail.com']
       end
     end
 end
