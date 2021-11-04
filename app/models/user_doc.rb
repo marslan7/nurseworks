5eef829077f5980415dc367a59a5795ca04412cf
hamza <hafizhamzasaleem4@gmail.com>
2021-11-05 00:31:52 +0500

    fixed

--- app/models/user_doc.rb
+++ app/models/user_doc.rb
@@ -1,12 +1,12 @@
 class UserDoc < ApplicationRecord
 
-    ThinkingSphinx::Callbacks.append(
-        self, :behaviours => [:real_time]
-    )
+    # ThinkingSphinx::Callbacks.append(
+    #     self, :behaviours => [:real_time]
+    # )
 
     belongs_to :user
     has_one_attached :attachment
-    
+
     TYPES = [
         "Drivers License", "SSN card", "CNA license", "Insurance card",
         "Doctors note", "Jury duty notification", "Covid vaccine appt",
@@ -18,7 +18,7 @@ class UserDoc < ApplicationRecord
 
     def acceptable_attachment
         return unless attachment.attached?
-        
+
         unless attachment.byte_size <= 5.megabyte
             errors.add(:attachment, "is too big. Must be less than 5 MB")
         end
