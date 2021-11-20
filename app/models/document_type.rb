cc2bb472897d937e797a16badcea50a54e2d956f
hamza <hafizhamzasaleem4@gmail.com>
2021-11-20 14:05:57 +0500

    validation added

--- app/models/document_type.rb
+++ app/models/document_type.rb
@@ -1,4 +1,15 @@
 class DocumentType < ApplicationRecord
   has_many :assign_supporting_doc
   has_many :user_docs
+
+  before_create :validate_sanitize
+
+  validates :alias, uniqueness: true
+  validates :name, uniqueness: true
+
+  private
+  def validate_sanitize
+    self.name = name.strip
+    self.alias = name.downcase.strip
+  end
 end
