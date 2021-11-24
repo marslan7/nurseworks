9e7dd39c025c148fcaa4639f0d74c57589ab13d9
hamza <hafizhamzasaleem4@gmail.com>
2021-11-24 16:52:53 +0500

    improvement

--- app/javascript/packs/custom.js
+++ app/javascript/packs/custom.js
@@ -3,14 +3,14 @@ document.addEventListener("turbolinks:load", function() {
     var startDate = $('.start-date')[0];
     var endDate = $('.end-date')[0];
     $('#support_request_support_request_type_id').change(function() {
-      if(this.value == 1) {
-        endDate.style.display = 'block';
-        startDate.style.display = 'block';
-      }
-      else{
-        endDate.style.display = 'none';
-        startDate.style.display = 'none';
-      }
+        if(this.value == 'Time off notification') {
+          endDate.style.display = 'block';
+          startDate.style.display = 'block';
+        }
+        else{
+          endDate.style.display = 'none';
+          startDate.style.display = 'none';
+        }
     })
   })
 })
