eacdd0776d7c9ca9ffccada3bd9392434045987a
Muhammad Arslan <arslan@arslan.com>
2021-12-31 20:47:18 +0500

    changes for deployments

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -29,7 +29,7 @@ Rails.application.configure do
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
   config.assets.compile = false
-  config.force_ssl = true
+  config.force_ssl = false
 
 
   # Enable serving of images, stylesheets, and JavaScripts from an asset server.
